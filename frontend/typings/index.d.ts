82c3d19525cc554785f431d3654af1facb36102c
黄泽文 <zw-huang22@mails.tsinghua.edu.cn>
2024-11-23 22:05:27 +0800

    clear cache

--- frontend/typings/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="./types/index.d.ts" />
-
-interface IAppOption {
-  globalData: {
-    userInfo?: WechatMiniprogram.UserInfo,
-    isDarkMode: boolean
-  }
-  userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
-}
