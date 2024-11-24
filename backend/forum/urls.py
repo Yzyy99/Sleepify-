e187298ce1117352c1931fab95131f4a8a04dda8
黄泽文 <zw-huang22@mails.tsinghua.edu.cn>
2024-11-24 17:17:49 +0800

    rewrite the views in APIView format, fix some issues and pass connection test now

--- backend/forum/urls.py
+++ backend/forum/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.get_forum_posts, name='get_forum_posts'),
-    path('post_pictures/', views.get_forum_picture, name='get_forum_picture'),
-    path('create_post/', views.create_forum_post, name='create_forum_post'),
-    path('create_picture/', views.create_forum_picture, name='create_forum_picture'),
+    path('posts/', views.GetForumPosts.as_view(), name='get_forum_posts'),
+    path('get_picture/', views.GetForumPicture.as_view(), name='get_forum_picture'),
+    path('create_post/', views.CreateForumPost.as_view(), name='create_forum_post'),
+    path('create_picture/', views.CreateForumPicture.as_view(), name='create_forum_picture'),
+    path('like_post/', views.LikeForumPost.as_view(), name='like_forum_post'),
+    path('reply_post/', views.ReplyForumPost.as_view(), name='reply_forum_post'),
 ]
