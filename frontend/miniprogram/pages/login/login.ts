156b64bccd9e540d7b29b4ad11ae6965a9eae957
Adam <129655857+Adam-THU@users.noreply.github.com>
2024-11-27 09:08:59 +0800

    fix: Remove code for simple login while testing (#47)

--- frontend/miniprogram/pages/login/login.ts
+++ frontend/miniprogram/pages/login/login.ts
@@ -45,8 +45,9 @@ Page({
 
   // 登录按钮逻辑
   onLogin() {
-    const { phoneNumber, password } = this.data;
-
+    const { phoneNumber, password } = this.data
+    
+    /* for simple login
     wx.showToast({
       title: '登录成功',
       icon: 'success',
@@ -60,7 +61,8 @@ Page({
         }, 2000);
       },
     });
-    
+    */
+
     // 简单的表单验证
     if (!phoneNumber || !password) {
       wx.showToast({
