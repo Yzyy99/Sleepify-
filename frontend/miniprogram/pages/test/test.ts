a138d5e1a57bc1a986f9ab0895c9dac39d0dc4fd
黄泽文 <zw-huang22@mails.tsinghua.edu.cn>
2024-12-11 01:12:54 +0800

    Screen check and Noise record in test page

--- frontend/miniprogram/pages/test/test.ts
+++ frontend/miniprogram/pages/test/test.ts
@@ -9,6 +9,13 @@ Page({
     count: 0,
     recordmode: false,
     recordoption: 0,
+    currentTime: 0,
+    totalTime: -1,
+    recorder_message: "噪声记录未启用",
+    currentDbValue: 0,
+    avgDbValue: 0,
+    maxDbValue: 0,
+    dbValues: [] as number[],
     // options: [
     //   { text: '直至睡眠结束', value: 'enable' },
     //   { text: '10min', value: 'enable' },
@@ -100,7 +107,7 @@ Page({
     // show a confirm
     wx.showModal({
       title: '提示',
-      content: '噪声检测需要启用麦克风功能，并将禁止屏幕自动熄灭，可能会消耗更多电量，是否继续？',
+      content: '噪声检测需要启用麦克风功能，并将禁止屏幕自动熄灭，可能会消耗更多电量。是否继续？',
       success: (res) => {
         if (res.confirm) {
           console.log('用户点击确定')
@@ -112,6 +119,85 @@ Page({
                 recordmode: true,
                 recordoption: res.tapIndex
               })
+              const timeList = [-1, 30, 60, 120, 180, 360]
+              this.setData({
+                totalTime: timeList[res.tapIndex]
+              })
+
+              var recorder = wx.getRecorderManager()
+              recorder.start({
+                duration: 60000,
+                sampleRate: 8000,
+                encodeBitRate: 16000,
+                format: 'PCM',
+                numberOfChannels: 1,
+                frameSize: 8,
+              })
+              recorder.onStart(() => {
+                console.log('recorder start')
+                this.setData({
+                  recorder_message: "噪声记录中"
+                })
+              })
+              recorder.onFrameRecorded((res) => {
+                var framebuffer = res.frameBuffer
+                // 分析音频流，得到分贝
+                const arrayBuffer = new Int32Array(framebuffer)
+                let sum = 0
+                for (let i = 0; i < arrayBuffer.length; i += 32) {
+                  sum += arrayBuffer[i] * arrayBuffer[i] / arrayBuffer.length * 32
+                }
+                // console.log('len: ', arrayBuffer.length)
+                // console.log('sum: ', sum)
+                const rms = Math.sqrt(sum)
+
+                // // get abs
+                // arrayBuffer.forEach((value, index) => {
+                //   arrayBuffer[index] = Math.abs(value)
+                // })
+                // const max = Math.max(...arrayBuffer)
+
+                const decibels = Math.round(10 * Math.log10(rms))
+
+                console.log('Current decibels: ', decibels)
+                this.setData({
+                  currentDbValue: decibels,
+                  dbValues: [...this.data.dbValues, decibels],
+                  maxDbValue: Math.max(this.data.maxDbValue, decibels),
+                })
+                this.setData({
+                  avgDbValue: Math.round((this.data.avgDbValue * (this.data.dbValues.length - 1) + decibels) / this.data.dbValues.length)
+                })
+              })
+              recorder.onError((res) => {
+                console.log(res.errMsg)
+                this.setData({
+                  recorder_message: "噪声记录中止"
+                })
+              })
+              recorder.onStop((res) => {
+                recorder.stop()
+                this.setData({
+                  currentTime: this.data.currentTime + 1
+                })
+                if (this.data.totalTime != -1 && this.data.currentTime >= this.data.totalTime) {
+                  this.setData({
+                    recordmode: false,
+                    recorder_message: "噪声记录结束"
+                  })
+                } else {
+                  recorder.start({
+                    duration: 60000,
+                    sampleRate: 8000,
+                    encodeBitRate: 16000,
+                    format: 'PCM',
+                    numberOfChannels: 1,
+                    frameSize: 8,
+                  })
+                }
+
+              })
+
             },
             fail: (res) => {
               console.log(res.errMsg)
